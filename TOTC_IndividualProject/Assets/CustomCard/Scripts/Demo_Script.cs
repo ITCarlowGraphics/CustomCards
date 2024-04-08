6ca051abc90083ec6e1dc1e79ec24fe1540cd62c
Lukas Kmitas <lukaskmitas@hotmail.co.uk>
2024-04-08 17:13:00 +0100

    Making custom card work in the main project

--- TOTC_IndividualProject/Assets/CustomCard/Scripts/Demo_Script.cs
+++ TOTC_IndividualProject/Assets/CustomCard/Scripts/Demo_Script.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Demo_Script : MonoBehaviour
 {
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
