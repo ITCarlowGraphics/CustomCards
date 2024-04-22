07fc36c7de6988965e59c8099eea05d945916179
Lukas Kmitas <lukaskmitas@hotmail.co.uk>
2024-04-22 14:59:23 +0100

    script updated to instantiate card

--- TOTC_IndividualProject/Packages/customcard/CustomCardConponent/Scripts/Instantiate_Card.cs
+++ TOTC_IndividualProject/Packages/customcard/CustomCardConponent/Scripts/Instantiate_Card.cs
@@ -6,20 +6,49 @@ using UnityEngine.UI;
 
 public class Instantiate_Card : MonoBehaviour
 {
+    private static Instantiate_Card instance;
 
-    private GameObject questionCardBackground;
-
-
-    void instantiateCard()
+    public static Instantiate_Card Instance
     {
-        GameObject prefab = Resources.Load<GameObject>("Prefabs/H_Theme_Question Card");
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<Instantiate_Card>();
+                if (instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(Instantiate_Card).Name);
+                    instance = singletonObject.AddComponent<Instantiate_Card>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void instantiateCard(string cardName, Color backgroundColor)
+    {
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/" + cardName);
         if (prefab != null)
         {
             GameObject instantiatedPrefab = Instantiate(prefab);
             instantiatedPrefab.transform.SetAsFirstSibling();
             instantiatedPrefab.transform.name = "Question Card";
 
-
+           
+            
         }
         else
         {
